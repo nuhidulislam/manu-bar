1de6b4b5e53ff69c9babe980d1150c0ab9f7b0bf
proabdulahad <144206282+proabdulahad@users.noreply.github.com>
2024-08-25 17:11:01 +0600

    added a manu-bar

---
+++ script.js
@@ -0,0 +1,10 @@
+var dd = document.getElementById('drop');
+let ss= document.getElementById('manu');
+
+function one(){
+    dd.style.display="block";
+}
+function open(){
+    // ss.style.display="block";
+    alert="hello bangladesh"
+}
